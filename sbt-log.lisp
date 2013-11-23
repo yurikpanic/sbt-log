60816a254598f757134654993c20c1085e773be4
Yuri Vishnevsky <vishnevsky@elsyton.com>
2013-11-23 09:22:42 +0200

    Now we can optionally print local vars in backtrace. And timestamp before "Ooops ..." message

--- sbt-log.lisp
+++ sbt-log.lisp
@@ -3,11 +3,18 @@
   (:use :cl)
   (:export :with-bt-log
            :*bt-log-stream*
+           :*invoke-debugger*
+           :*print-vars*
+           :*print-timestamp*
            :print-bt))
 
 (in-package :sbt-log)
 
 (defvar *invoke-debugger* nil)
+
+(defvar *print-vars* nil)
+
+(defvar *print-timestamp* t)
 
 (defvar *bt-log-stream* t)
 
@@ -27,26 +34,48 @@
       (format *bt-log-stream* "~%      ~S" path)
       (print-file-pos src))))
 
+(defun print-local-vars (frame)
+  (let ((fun (sb-di:frame-debug-fun frame)))
+    (dolist (var (sb-di:debug-fun-lambda-list fun))
+      (format *bt-log-stream* "~%          ")
+      (when (typep var 'list)
+        (format *bt-log-stream* "~S " (car var))
+        (setf var (second var)))
+      (format *bt-log-stream* "~A = ~S" (sb-di:debug-var-symbol-name var)
+              (handler-case
+                  (sb-di:debug-var-valid-value var frame)
+                (sb-di:invalid-value () :unknown))))))
+
 (defun print-bt ()
   (loop for frame = (sb-di:top-frame) then (sb-di:frame-down frame)
      until (null frame)
      do (let* ((fun (sb-di:frame-debug-fun frame))
                (fun-name (sb-di:debug-fun-name fun)))
           (format *bt-log-stream* "~D: ~S" (sb-di:frame-number frame) fun-name)
+          (when *print-vars* (print-local-vars frame))
           (print-source-location fun-name)
           (format *bt-log-stream* "~%"))))
 
+(defun print-timestartmp ()
+  (let ((time (multiple-value-list (get-decoded-time))))
+    (format *bt-log-stream* "~4,'0D-~2,'0d-~2,'0d ~2,'0d:~2,'0d:~2,'0d"
+            (sixth time) (fifth time) (fourth time) (third time) (second time) (first time))))
+
 (defun sbt-debug (condition dh)
   (declare (ignore dh))
+  (when *print-timestamp*
+    (print-timestartmp)
+    (princ " " *bt-log-stream*))
   (format *bt-log-stream* "Ooops: ~A~%" condition)
   (print-bt)
   (if *invoke-debugger*
       (funcall *invoke-debugger* condition)
       (invoke-debugger condition)))
 
-(defmacro with-bt-log ((&key (out-stream t) invoke-debugger) &body body)
+(defmacro with-bt-log ((&key (out-stream t) invoke-debugger print-vars) &body body)
   `(let ((*debugger-hook* #'sbt-debug)
          (*bt-log-stream* ,out-stream)
-         (*invoke-debugger* ,invoke-debugger))
+         (*invoke-debugger* ,invoke-debugger)
+         (*print-vars* ,print-vars))
      ,@body))
 
